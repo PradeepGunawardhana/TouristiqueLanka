ef752413e6ae0ff073b663dfbdba177bc5369e5a
Rangana Pradeep <123355131+PradeepGunawardhana@users.noreply.github.com>
2026-02-08 20:55:35 +0530

    Remove TypeScript - convert to pure JavaScript

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,8 +2,8 @@ import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 import flowbiteReact from "flowbite-react/plugin/vite";
-
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss(), flowbiteReact()],
+    base: '/TouristiqueLanka/',
+    plugins: [react(), tailwindcss(), flowbiteReact()],
 });
